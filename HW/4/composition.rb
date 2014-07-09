a1295105c5798e61f6c6c0533d9adf1787fe0eec
Josiah Faulkner-Harrington <jfaulknerharrington6996@sohamcollege.org.uk>
2014-07-09 23:41:27 +0100

    Update composition.rb
    i have done a sample of the 'baa baa black sheep' song with comments

--- HW/4/composition.rb
+++ HW/4/composition.rb
@@ -1,1 +1,92 @@
-#paste code here
+#baa baa black sheep
+#by josiah faulkner-harrington
+
+#line 1 - the music lines are numbered
+#2x c - each is commented what note it is and how many
+2.times do #loops have been used to make more efficient
+	play 48 #ths play funtion alows to play a note specific number
+	sleep 0.5 #sleep is a funtion used to make pauses
+end 
+#2x G 
+2.times do #some loops have differnet amounts of times running the code
+	play 55
+	sleep 0.5
+end
+#A
+play 57 
+sleep 0.24
+#2x A
+2.times do #each loop has a sleep funtion and a play funtion for pauses and playing sounds
+	play 57
+	sleep 0.24
+end
+#A
+play 57
+sleep 0.2
+#G
+play 55
+sleep 1
+
+#line 2   
+#2x F
+2.times do
+	play 53
+	sleep 0.5
+end
+#2x E
+2.times do
+	play 52
+	sleep 0.5
+end
+#2x D
+2.times do
+	play 50
+	sleep 0.55
+end
+#C
+play 48
+sleep 1
+
+#line 3
+
+play 53
+sleep 0.6
+#2x F
+2.times do
+	play 53
+	sleep 0.3
+end
+#2x E
+2.times do
+	play 52
+	sleep 0.3
+end
+sleep 0.4
+#2x D
+play 0.3
+2.times do 
+	play 55
+	sleep 0.4
+end
+play 48
+sleep 1
+
+#line 4
+
+#3x F
+3.times do
+	play 53
+	sleep 0.4
+end
+#3x E
+3.times do
+	play 52
+	sleep 0.4
+end
+#3x D
+3.times do
+	play 50
+	sleep 0.3
+end
+#C
+play 48
